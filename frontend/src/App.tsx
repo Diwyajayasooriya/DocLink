35fcd06ed657cb533f647591c66b115b458ffeaf
Deeshan <e21444@eng.pdn.ac.lk>
2025-06-28 00:27:29 +0530

    Choose Doctor Component Added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-// import DoctorDetails from "./pages/DoctorDetails";
-// import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -17,9 +15,8 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} /> {/* Now shows only Header + SearchFilterBar */}
-          {/* <Route path="/doctor/:id" element={<DoctorDetails />} /> */}
-          {/* <Route path="*" element={<NotFound />} /> */}
+          <Route path="/" element={<Index />} /> 
+          
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
